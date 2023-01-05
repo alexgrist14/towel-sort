929ee345e04fbcf15048b61da90b996f03b47d0a
Daniil Stasulevich <alexgrist133@gmail.com>
2023-01-05 13:10:31 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
-// You should implement your task here.
+function ascSort(a, b) {
+    return a - b;
+}
+
+function descSort(a, b) {
+    return b - a;
+}
+
+function SortAnArray(arr) {
+    let newArr = [];
+    arr.forEach((item, index) => {
+        newArr.push(item.sort(index % 2 ? descSort : ascSort));
+    });
+    return newArr;
+}
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(arguments[0]=== undefined) return [];
+    if(!matrix.length) return [];
+
+    return SortAnArray(matrix).flat();
 }
